bec449addf561b7d06880ae4a46a8417e8c7bd84
duyenpham4622 <duyenpham4622@gmail.com>
2024-09-25 07:42:31 +0700

    Add files via upload

---
+++ src/tuan4_bai1_ChuyenXe/tuan4_bai1_ChuyenXe/NgoaiThanh.java
@@ -0,0 +1,43 @@
+package tuan4_bai1_ChuyenXe;
+
+public class NgoaiThanh extends ChuyenXe {
+	
+	private String noiDen;
+	private int soNgayDiDuoc;
+
+	// ------------------ contructor đầy đủ tham số-----------------------------------------
+
+	public NgoaiThanh(String maChuyenXe, String hoTen, int soXe, double doanhThu, String noiDen, int soNgayDiDuoc) {
+		//super gọi contructor của lớp cha
+		super(maChuyenXe, hoTen, soXe, doanhThu);
+		this.noiDen = noiDen;
+		this.soNgayDiDuoc = soNgayDiDuoc;
+	}
+	// ------------------ contructor mặc định -----------------------------
+	public NgoaiThanh() {
+		super();
+	}
+	// -------------------- begin setget -----------------------------------------
+	public String getNoiDen() {
+		return noiDen;
+	}
+
+	public void setNoiDen(String noiDen) {
+		this.noiDen = noiDen;
+	}
+
+	public int getSoNgayDiDuoc() {
+		return soNgayDiDuoc;
+	}
+
+	public void setSoNgayDiDuoc(int soNgayDiDuoc) {
+		this.soNgayDiDuoc = soNgayDiDuoc;
+	}
+	// --------------------------end ---------------------------------------------
+	@Override
+	public String toString() {
+		return super.toString() + "\n Nơi đến: " + getNoiDen() + "\n Ngày đi được: " + getSoNgayDiDuoc();
+	}
+
+
+}
